6b06db5760085dab20c98e4293ee23fbd76f794e
Santosh Chauhan <santoshch00099@gmail.com>
2024-07-06 23:20:34 +0530

    first commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./userSlice";
 import appSlice from "./appSlice";
 
 const store = configureStore({
   reducer: {
-    user: userSlice.reducer,
     app: appSlice.reducer,
   },
 });
